0152c0b7a59b320b51cef23273c7b335ea38f4ab
Vova Tkach <vladimirok5959@gmail.com>
2019-12-22 23:39:11 +0200

    Rename var

--- worker/worker.go
+++ worker/worker.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Worker struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-	chDone chan bool
-	stop   bool
+	ctx     context.Context
+	cancel  context.CancelFunc
+	chDone  chan bool
+	stopped bool
 }
 
 type Callback func(ctx context.Context, w *Worker)
@@ -36,11 +36,11 @@ func (this *Worker) doit(f func(ctx context.Context, w *Worker)) *Worker {
 }
 
 func (this *Worker) Shutdown(ctx context.Context) error {
-	if this.stop {
+	if this.stopped {
 		return nil
 	}
 
-	this.stop = true
+	this.stopped = true
 
 	ctxb := ctx
 	if ctxb == nil {
